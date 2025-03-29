5a0975e7398113184fdd48dcf4d1777cdbae0d32
salvador-lopez <s.lopezreyes255@gmail.com>
2025-03-29 14:42:32 +0100

    Remove hello-world endpoint and create healthz one

---
+++ src/routes/healthz.ts
@@ -0,0 +1,18 @@
+import {Request, Response, Router} from "express";
+
+const router = Router();
+
+/**
+ * @openapi
+ * /healthz:
+ *   get:
+ *     summary: Healthz endpoint
+ *     responses:
+ *       200:
+ *         description: Return ok determining that the api is alive. Useful for Liveness probes
+ */
+router.get('/healthz', (req: Request, res: Response) => {
+    res.send('ok');
+});
+
+module.exports = router;
