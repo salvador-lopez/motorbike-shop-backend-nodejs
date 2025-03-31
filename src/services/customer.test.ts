a2b1fd3a4993ec19a88a4073a02ee50ed530500d
salvador-lopez <s.lopezreyes255@gmail.com>
2025-03-31 12:36:23 +0200

    Transform repository UniqueConstraintError to DomainConflictError in the customer.Create application service

--- src/services/customer.test.ts
+++ src/services/customer.test.ts
@@ -4,6 +4,7 @@ import {v4 as UUID} from "uuid";
 import { mock, mockReset } from 'jest-mock-extended';
 import {DomainConflictError} from "../domain/errors";
 import {EntityId, Email, Credit} from "../domain/common";
+import {UniqueConstraintError} from "../database/errors";
 
 describe('CustomerService', () => {
     const mockRepository = mock<CustomerRepository>();
@@ -44,6 +45,19 @@ describe('CustomerService', () => {
         const resultPromise = customerService.create(id, email);
 
         await expect(resultPromise).rejects.toThrow("Invalid email: invalid-email");
+        await expect(resultPromise).rejects.toBeInstanceOf(DomainConflictError);
+    });
+
+    it('should throw DomainConflictError when repository throw UniqueConstraintError', async () => {
+        const id = UUID();
+        const email = "email@example.com";
+        mockRepository.create.mockImplementationOnce(async () => {
+            throw new UniqueConstraintError("unique constraint error");
+        });
+
+        const resultPromise = customerService.create(id, email);
+
+        await expect(resultPromise).rejects.toThrow("unique constraint error");
         await expect(resultPromise).rejects.toBeInstanceOf(DomainConflictError);
     });
 });
--- src/services/customer.ts
+++ src/services/customer.ts
@@ -1,5 +1,7 @@
 import {Customer, CustomerRepository} from "../domain/customer";
 import {EntityId, Email} from "../domain/common";
+import {UniqueConstraintError} from "../database/errors";
+import {DomainConflictError} from "../domain/errors";
 
 export class CustomerService {
     private repository: CustomerRepository;
@@ -11,6 +13,13 @@ export class CustomerService {
     async create(id: string, email: string): Promise<void> {
         const newCustomer = new Customer(new EntityId(id), new Email(email));
 
-        await this.repository.create(newCustomer);
+        try {
+            await this.repository.create(newCustomer);
+        } catch (error) {
+            if (error instanceof UniqueConstraintError) {
+                throw new DomainConflictError(error.message);
+            }
+            throw error;
+        }
     }
 }
