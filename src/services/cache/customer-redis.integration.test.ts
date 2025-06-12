e5acf2fff9f4f21f30eb56a044e5dab6c1697ee7
Lucas Pirez <lucaspirez42@gmail.com>
2025-06-12 11:25:16 -0300

    Add ttl expires test, refactor redis.integration.test

--- src/services/cache/customer-redis.integration.test.ts
+++ src/services/cache/customer-redis.integration.test.ts
@@ -3,13 +3,23 @@ import {RedisCustomerCache} from "./customer-redis";
 import {CustomerDTO} from "../customer";
 import { v4 as UUID} from 'uuid';
 
-let redisClient: RedisClientType;
-let redisCustomer: RedisCustomerCache;
-let prefixKey: string = 'customers:'
+describe("Customer Redis Integration Test", () => {
+  const id = UUID();
+  let redisClient: RedisClientType;
+  let redisCustomer: RedisCustomerCache;
+  let prefixKey: string = 'customers:'
 
-afterEach(async () => {
-   await redisClient.flushAll();
-});
+  function generateKey(paramId: string) {
+    return `${prefixKey}${paramId}`
+  }
+
+  afterEach(async () => {
+    const keys = await redisClient.keys(`${prefixKey}*`);
+    if (keys.length > 0) {
+      await redisClient.del(keys);
+    }
+
+  });
 
 beforeAll(async () => {
 redisClient =  createClient({
@@ -22,15 +32,12 @@ redisClient =  createClient({
 
 afterAll(async () => {
     await redisClient.quit();
-
 });
 
-describe("Customer Redis Integration Test", () => {
-
     it('get by id',async()=> {
-        const customerDTO = new CustomerDTO(UUID(),'email@example.com',0);
+        const customerDTO = new CustomerDTO(id,'email@example.com',0);
 
-        await redisClient.set(`${prefixKey}${customerDTO.id}`,JSON.stringify(customerDTO));
+        await redisClient.set(generateKey(customerDTO.id),JSON.stringify(customerDTO));
 
         expect(await redisCustomer.get(customerDTO.id)).toEqual(
             customerDTO
@@ -38,29 +45,36 @@ describe("Customer Redis Integration Test", () => {
     })
 
     it('Should cache customer dto', async()=> {
-        const customerDTO = new CustomerDTO(UUID(),'email@example.com',0);
+        const customerDTO = new CustomerDTO(id,'email@example.com',0);
 
         await redisCustomer.set(customerDTO, 10);
 
-        expect(await redisClient.get(`${prefixKey}${customerDTO.id}`)).toEqual(
-            JSON.stringify(customerDTO)
-        )
+      expect(await redisClient.get(generateKey(customerDTO.id))).toEqual(
+        JSON.stringify(customerDTO)
+      )
     })
 
 
     it('Should get all customers dto', async()=> {
-        const customerDTO1 = new CustomerDTO(UUID(),'email1@example.com',1);
+        const customerDTO1 = new CustomerDTO(id,'email1@example.com',1);
         const customerDTO2 = new CustomerDTO(UUID(),'email2@example.com',2);
-        const customerDTO3 = new CustomerDTO(UUID(),'email3@example.com',3);
 
-        await redisClient.set(`${prefixKey}${customerDTO1.id}`,JSON.stringify(customerDTO1));
-        await redisClient.set(`${prefixKey}${customerDTO3.id}`,JSON.stringify(customerDTO3));
-        await redisClient.set(`${prefixKey}${customerDTO2.id}`,JSON.stringify(customerDTO2));
+        await redisClient.set(generateKey(customerDTO1.id),JSON.stringify(customerDTO1));
+        await redisClient.set(generateKey(customerDTO2.id),JSON.stringify(customerDTO2));
 
         const result = await redisCustomer.getAll();
         expect(result.sort((a,b) => a.availableCredit - b.availableCredit)).toEqual(
-            [customerDTO1, customerDTO2, customerDTO3].sort((a,b) => a.availableCredit + b.availableCredit)
+            [customerDTO1,customerDTO2].sort((a,b) => a.availableCredit - b.availableCredit)
         )
     })
 
-})
+    it('Should set correct TTL', async () => {
+      const customerDTO = new CustomerDTO(id, 'email@example.com', 0)
+      const ttl = 10
+      await redisCustomer.set(customerDTO, ttl)
+
+      const ttlSet = await redisClient.ttl(generateKey(customerDTO.id))
+
+      expect(ttlSet).toEqual(ttl)
+    })
+  })
